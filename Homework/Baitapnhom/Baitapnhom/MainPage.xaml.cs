9397fd6c13d867c6bc2c60150a4a44ddda6e833a
minh <65264453+lehoangminhfpt2020@users.noreply.github.com>
2021-05-31 17:57:58 +0700

    baitapnhom

---
+++ Homework/Baitapnhom/Baitapnhom/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Baitapnhom
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            MyFrame.Navigate(typeof(Pages.Home));
+            lighthome.Background = oncolor.Background;
+            lightEat.Background= mycolor.Background;
+            lightCollection.Background = mycolor.Background;
+            lightDelivery.Background = mycolor.Background;
+            lightTakeAway.Background = mycolor.Background;
+            lightDriverPayment.Background = mycolor.Background;
+            lightCustomers.Background = mycolor.Background;
+        }
+
+        private void homebutton_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.Navigate(typeof(Pages.Home));
+            lighthome.Background = oncolor.Background;
+            lightEat.Background = mycolor.Background;
+            lightCollection.Background = mycolor.Background;
+            lightDelivery.Background = mycolor.Background;
+            lightTakeAway.Background = mycolor.Background;
+            lightDriverPayment.Background = mycolor.Background;
+            lightCustomers.Background = mycolor.Background;
+            
+        }
+
+        private void eatbutton_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.Navigate(typeof(Pages.Eat));
+            lighthome.Background = mycolor.Background;
+            lightEat.Background = oncolor.Background;  
+            lightCollection.Background = mycolor.Background;
+            lightDelivery.Background = mycolor.Background;
+            lightTakeAway.Background = mycolor.Background;
+            lightDriverPayment.Background = mycolor.Background;
+            lightCustomers.Background = mycolor.Background;
+        }
+    }
+}
